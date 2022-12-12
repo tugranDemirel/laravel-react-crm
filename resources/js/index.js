0fccc19f15d37ac4cd28914e56187b51191490f6
Tugran Demirel <demireltugran66@gmail.com>
2022-12-12 20:28:59 +0300

    react running

---
+++ resources/js/index.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+class Index extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Index</h1>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Index />, document.getElementById("index"));

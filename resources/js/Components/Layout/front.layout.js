3d3616fa3ada3a58d168c5996cbe4557285ebcfe
Tugran Demirel <demireltugran66@gmail.com>
2022-12-13 17:38:01 +0300

    template create

--- resources/js/Components/Layout/front.layout.js
+++ resources/js/Components/Layout/front.layout.js
@@ -2,7 +2,11 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import {inject, observer} from 'mobx-react'
 import {useHistory} from "react-router-dom";
-
+import {
+    NavDropdown,
+    Container,
+    Nav,
+    Navbar} from 'react-bootstrap';
 const Layout = (props) => {
     props.AuthStore.getToken();
     const history = useHistory()
@@ -38,7 +42,34 @@ const Layout = (props) => {
     }
     return (
         <>
-            <div><button onClick={logout}>Çıkış</button></div>
+            <Navbar bg="light" expand="lg">
+                <Container >
+                    <Navbar.Brand href="#">tStock</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="navbarScroll" />
+                    <Navbar.Collapse id="navbarScroll">
+                        <Nav
+                            className="me-auto my-2 my-lg-0"
+                            style={{ maxHeight: '100px' }}
+                            navbarScroll
+                        >
+                            <Nav.Link href="/">Yönetim Paneli</Nav.Link>
+                            <Nav.Link href="/urunler">Ürünler</Nav.Link>
+
+                            <Nav.Link href="#" disabled>
+                                Link
+                            </Nav.Link>
+                        </Nav>
+                            <NavDropdown title={user.name} id="navbarScrollingDropdown">
+                                <NavDropdown.Item href="#action3">Profil Düzenle</NavDropdown.Item>
+                                <NavDropdown.Item href="#action4">
+                                    Şifre Değiştir
+                                </NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={logout} >Çıkış</NavDropdown.Item>
+                            </NavDropdown>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
             <div>{props.children}</div>
         </>
     )
--- resources/js/index.js
+++ resources/js/index.js
@@ -4,6 +4,8 @@ import Main from "./Router";
 import {BrowserRouter, Route} from "react-router-dom";
 import {Provider} from "mobx-react";
 import Store from './Store/index'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class Index extends Component {
     render() {
         return (
